ea152474d352f44d4976bd921dfc35a547728003
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2025-08-26 14:52:54 -0300

    feat(models): create classmethods

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
-from main import db, bcrypt
+from extensions import db, bcrypt
 
 class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    emial = db.Column(db.String(12), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -15,6 +15,26 @@ class UserModel(db.Model):
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
-    
+
     def json(self):
-        return {'id': self.id, 'username': self.username, 'email': self.email}
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
