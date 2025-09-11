0b420f87236b81396666f29d779c593e879ac524
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2025-09-11 15:25:29 -0300

    feat(main): main updates
    +apply restx in api
    +import auth in main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask, jsonify, request
+from flask import Flask
 from dotenv import load_dotenv
+from flask_restx import Api
 
 from extensions import db, bcrypt
-from models import UserModel
 
 def create_app():
     load_dotenv()
@@ -17,43 +17,24 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    api = Api(app,
+              version='1.0',
+              title='Email Sender API',
+              description='Uma API para envio de emails e gerenciamento de usuários.')
+
     db.init_app(app)
     bcrypt.init_app(app)
 
+    from resources.auth import auth_ns
+    
+    api.add_namespace(auth_ns, path='/auth')
+
     with app.app_context():
         db.create_all()
-
-    @app.route('/')
-    def index():
-        return jsonify({"status": "API is running!"})
-
-    @app.route('/register', methods=['POST'])
-    def register():
-        data = request.get_json()
-
-        if not data or not data.get('username') or not data.get('password') or not data.get('email'):
-            return jsonify({"message": "Faltando username, email ou password"}), 400
-
-        if UserModel.find_by_username(data['username']):
-            return jsonify({"message": "Um usuário com esse nome já existe"}), 400
-        if UserModel.find_by_email(data['email']):
-            return jsonify({"message": "Um usuário com esse email já existe"}), 400
-
-        new_user = UserModel(
-            username=data['username'],
-            email=data['email'],
-            password=data['password']
-        )
-
-        try:
-            new_user.save_to_db()
-            return jsonify({"message": "Usuário criado com sucesso."}), 201
-        except Exception as e:
-            print(e)
-            return jsonify({"message": "Ocorreu um erro ao criar o usuário."}), 500
             
     return app
 
+# O bloco if __name__ == '__main__' continua o mesmo
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
