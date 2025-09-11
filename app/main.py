0762e4448b3d2e2deda19283ebb22677243f139f
Dudu Heylens <54040715+dudu1337@users.noreply.github.com>
2025-09-11 14:32:46 -0300

    feat(main): refactoring main
    +remove login from main
    +change mysqlconnector to pymysql

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
-from flask_jwt_extended import create_access_token, JWTManager
+
 from extensions import db, bcrypt
 from models import UserModel
 
@@ -14,13 +14,11 @@ def create_app():
     db_name = os.environ.get('MYSQL_DATABASE')
     db_host = 'db' 
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["JWT_SECRET_KEY"] = "sua-chave-secreta-super-dificil"
 
     db.init_app(app)
     bcrypt.init_app(app)
-    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
@@ -54,21 +52,6 @@ def create_app():
             print(e)
             return jsonify({"message": "Ocorreu um erro ao criar o usuário."}), 500
             
-
-
-    
-    @app.route('/login', methods=['POST'])
-    def login():
-        data = request.get_json()
-
-        user = UserModel.find_by_username(data['username'])
-
-        if user and user.check_password(data['password']):
-            access_token = create_access_token(identity=user.id)
-            return jsonify(access_token=access_token)
-        return jsonify({"message": "Credenciais inválidas"}), 401
-
-
     return app
 
 if __name__ == '__main__':
